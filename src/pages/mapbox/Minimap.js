57b4e7ce4146f0a1a1d5ed7381daf2f1b678216d
xushanpei <xushanpei@hiynn.com>
2019-09-17 09:23:26 +0800

    修改minimap样式和mapbox样式统一

--- src/pages/mapbox/Minimap.js
+++ src/pages/mapbox/Minimap.js
@@ -18,7 +18,7 @@ Minimap.prototype = Object.assign({}, mapboxgl.NavigationControl.prototype, {
     id: "mapboxgl-minimap",
     width: "320px",
     height: "180px",
-    style: "mapbox://styles/mapbox/streets-v8",
+    style: "mapbox://styles/mapbox/light-v10",
     center: [0, 0],
     zoom: 6,
 
